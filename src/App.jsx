69267b1254e5e0037ded06306d600828e00c434d
srini255 <srinitamil17@gmail.com>
2025-02-05 19:05:33 +0530

    addded api key

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import hero from './assets/hero.png';
 import Search from './components/Search';
 import { useState,useEffect } from 'react';
+
+const API_BASE_URL ='https://api.themoviedb.org/3' ;
+const  API_KEY=import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  },
+}
+
+
+
 function App() {
   const [searchterm, setSearchterm] = useState('')
 
