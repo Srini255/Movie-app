fec6d36cbd232c00558661c56ecf502fb91f6c97
Spidey255 <worksrini255@gmail.com>
2025-02-06 18:26:03 +0530

    get movies data from api and shows it to screen

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,38 @@ const API_OPTIONS = {
 
 
 function App() {
-  const [searchterm, setSearchterm] = useState('')
+  const [searchterm, setSearchterm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchMovies = async() =>{
+    try{
+      setIsLoading(true);
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity,desc`;
 
+      const response=await fetch(endpoint,API_OPTIONS);
+      if(!response.ok){
+          throw new Error('Failed to fetch movies')
+        }
+        const data = await response.json();
+
+        if(data.response==='False'){
+          setErrorMessage(data.Error||'Failed to fetch data');
+          setMovieList([]);
+          return;
+        }
+        console.log(data);
+        setMovieList(data.results||[]);
+    }
+    catch(e){
+      console.error(err.msg);
+    }
+    finally{
+      setIsLoading(false);
+    }
+  }
   useEffect(()=>{
-
+    fetchMovies();
   },[])
 
   return (
@@ -30,8 +58,19 @@ function App() {
             <header>
                 <img src={hero} alt="Hero Banner"/>
                 <h1>Find <span className="text-gradient">Movies </span> you'll enjoy without the Hassle</h1>
+                <Search searchterm={searchterm} setSearchterm={setSearchterm}/>
             </header>
-           <Search searchterm={searchterm} setSearchterm={setSearchterm}/>
+           <section>
+            <h2>All Movies</h2>
+            {isLoading?( <p className="text-white">Loading...</p>):
+              errorMessage?(<p className="text-red-500">{errorMessage}</p>):(
+                <ul>
+                  {movieList.map((movie)=>{
+                    return <li className="text-white" key={movie.id}>{movie.title}</li>
+                  })}
+                </ul>
+              )}
+           </section>
         </div>
     </>
   )
