00d51288b3c104e9cbd63ec8a4dc34d7cf05ffaf
Spidey255 <worksrini255@gmail.com>
2025-02-06 19:24:30 +0530

    added search api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import hero from './assets/hero.png';
 import Search from './components/Search';
 import { useState,useEffect } from 'react';
+import Spinner from './components/Spinner';
+import { MovieCard } from './components/MovieCard';
 
 const API_BASE_URL ='https://api.themoviedb.org/3' ;
 const  API_KEY=import.meta.env.VITE_TMDB_API_KEY;
@@ -21,10 +23,10 @@ function App() {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const fetchMovies = async() =>{
+  const fetchMovies = async(query) =>{
     try{
       setIsLoading(true);
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity,desc`;
+      const endpoint = query?`${API_BASE_URL}/search/movie?query=${query}`:`${API_BASE_URL}/discover/movie?sort_by=popularity,desc`;
 
       const response=await fetch(endpoint,API_OPTIONS);
       if(!response.ok){
@@ -48,8 +50,8 @@ function App() {
     }
   }
   useEffect(()=>{
-    fetchMovies();
-  },[])
+    fetchMovies(searchterm);
+  },[searchterm])
 
   return (
     <>
@@ -60,14 +62,14 @@ function App() {
                 <h1>Find <span className="text-gradient">Movies </span> you'll enjoy without the Hassle</h1>
                 <Search searchterm={searchterm} setSearchterm={setSearchterm}/>
             </header>
-           <section>
-            <h2>All Movies</h2>
-            {isLoading?( <p className="text-white">Loading...</p>):
+           <section className="all-movies">
+            <h2 className="mt-[40px]">All Movies</h2>
+            {isLoading?( <Spinner/>):
               errorMessage?(<p className="text-red-500">{errorMessage}</p>):(
                 <ul>
-                  {movieList.map((movie)=>{
-                    return <li className="text-white" key={movie.id}>{movie.title}</li>
-                  })}
+                  {movieList.map((movie)=>(
+                    <MovieCard movie={movie}/>
+                  ))}
                 </ul>
               )}
            </section>
